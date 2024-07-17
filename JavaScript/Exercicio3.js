ab101dfa686fdca75e4a939028ecd44aeb2b7e61
João Guilherme <joaosilvabarbosa001@gmail.com>
2024-07-17 16:59:55 -0300

    adicionando exercicio 3 quase finalizado

---
+++ JavaScript/Exercicio3.js
@@ -0,0 +1,50 @@
+//Global variaveis
+let salvar = [];
+let i = 1;
+// Function object Pessoa
+function Pessoa(nome, idade) {
+  this.nome = nome;
+  this.idade = idade;
+}
+function MaiorMenor(nome, outroNome) {
+  if (this.nome !== this.outroNome) {
+    console.log(`${nome} e ${outroNome} são nomes diferentes!`);
+  } else {
+    console.log(`${nome} e ${outroNome} tem nomes iguais!`);
+  }
+}
+//loop para cadastrar novas pessoas
+while (i != 0) {
+  valor = parseInt(
+    prompt('Escolhas uma das opções: 1 - Cadastrar, e 2 - Sair')
+  );
+  switch (valor) {
+    case 1:
+      let nome = prompt('Insira um nome:');
+      let idade = prompt('Insira uma idade:');
+      let novaPessoa = new Pessoa(nome, idade);
+      salvar.push(novaPessoa);
+      break;
+    case 2:
+      alert('Obrigado por usar nosso sistema!');
+      console.log('Programa Encerrado!');
+      console.log('Dados capturados:');
+      for (let novaPessoa of salvar) {
+        console.log(novaPessoa);
+      }
+      for (let a = 0; a < salvar.length; a++) {
+        for (let b = a + 1; b < salvar.length; b++) {
+          MaiorMenor(salvar[a], salvar[b]);
+        }
+      }
+      i = 0;
+      break;
+    default:
+      alert(
+        'Nenhum serviço selecionado o programa foi encerrado! Confira os dados no console!'
+      );
+      console.log('nenhum serviço foi selecionado!');
+      console.log('Dados capturados:');
+      i = 0;
+  }
+}
