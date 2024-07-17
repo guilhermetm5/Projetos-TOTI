3d788712d4e6da998de28f50d438c7da3a0daeff
João Guilherme <joaosilvabarbosa001@gmail.com>
2024-07-17 08:45:50 -0300

    exercicios 1 e 2 de JavaScript

---
+++ JavaScript/Exercicio1.js
@@ -0,0 +1,15 @@
+alert('Faça um cadastro antes acessar a página!');
+
+let nome = prompt('Seu primeiro nome:');
+let sobrenome = prompt('Seu sobrenome:');
+let idade = prompt('Sua idade:');
+
+alert('Cadastrado com sucesso! Veja suas informações no console!');
+console.log(`Cadastrado! informações do cadastrado: `);
+
+Number(idade);
+console.table({
+  nome,
+  sobrenome,
+  idade,
+});
