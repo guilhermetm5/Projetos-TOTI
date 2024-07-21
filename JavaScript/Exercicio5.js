a804b4593db6760721f13828e6697f97d693eb40
João Guilherme <joaosilvabarbosa001@gmail.com>
2024-07-21 16:44:08 -0300

    add function para desabilitar o button

--- JavaScript/Exercicio5.js
+++ JavaScript/Exercicio5.js
@@ -1,13 +1,19 @@
-//chamada da API
-
 //Selecionando elementos da página
-let button = document.querySelector("#enviar");
-let cep = document.querySelector("#cep");
-let lista = document.querySelector("#lista");
+let button = document.querySelector('#enviar');
+let cep = document.querySelector('#cep');
+let lista = document.querySelector('#lista');
 let dataCEP;
 
 // Funções
 
+//desabilitando o botão enviar
+function enviarDisabled() {
+  button.disabled = true;
+}
+//desabilitando o button para nada ser enviado antes da verificação
+enviarDisabled();
+
+//chamada da API
 function conexao(cep) {
   fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
     .then((resp) => resp.json())
@@ -15,22 +21,24 @@ function conexao(cep) {
       dataCEP = data;
       exibir(dataCEP);
     })
-    .catch((error) => console.error("Requisição falhou ", error));
+    .catch((error) => console.error('Requisição falhou ', error));
 }
+
+//Salvando conteúdo de input
 
 function salvar(event) {
   event.preventDefault();
-  let li = document.createElement("li");
+  let li = document.createElement('li');
   li.textContent = `Você escolheu: ${cep.value}`;
   lista.appendChild(li);
   conexao(cep);
 }
+//exibindo na página o dados
 function exibir(dataCEP) {
-  let li = document.createElement("li");
+  let li = document.createElement('li');
   li.textContent = `Localidade: ${dataCEP.localidade}, Logradouro: ${dataCEP.logradouro}, Bairro: ${dataCEP.bairro}`;
-  lista.appendChild(li)
+  lista.appendChild(li);
 }
 
 // Eventos
-
-button.addEventListener("click", salvar);
+button.addEventListener('click', salvar);
