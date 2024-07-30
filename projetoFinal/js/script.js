4dd7f3611fc5299215c904e43e5175e4bac6691c
joaosilva <joaosilvabarbosa001@gmail.com>
2024-07-30 16:08:12 -0400

    adicionando bootstrap

---
+++ projetoFinal/js/script.js
@@ -0,0 +1,8 @@
+console.log(busca.value, input1.value, input2.value);
+
+function exibe() {
+  let busca = document.getElementById("nome").value;
+  let input1 = document.getElementById("input1").value;
+  let input2 = document.getElementById("input2").value;
+  console.log("ola " + busca, input1, input2);
+}
