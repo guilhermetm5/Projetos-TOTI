8502396c44a6a4731afe62a719fbff0cfcadcd15
joaosilva <joaosilvabarbosa001@gmail.com>
2024-07-30 17:10:46 -0400

    consertando erro de concatenação

--- projetoFinal/js/script.js
+++ projetoFinal/js/script.js
@@ -1,8 +1,27 @@
-console.log(busca.value, input1.value, input2.value);
+console.log("Projeto Turma 48");
 
-function exibe() {
-  let busca = document.getElementById("nome").value;
-  let input1 = document.getElementById("input1").value;
-  let input2 = document.getElementById("input2").value;
-  console.log("ola " + busca, input1, input2);
+function enviarFormulario() {
+  const name = document.getElementById("name").value;
+  const cpf = document.getElementById("cpf").value;
+  const nascimento = document.getElementById("nascimento").value;
+  const email  = document.getElementById("email").value;
+  const telefone = document.getElementById("telefone").value;
+  const pais = document.getElementById("pais").value;
+  const estado = document.getElementById("estado").value;
+  const cidade = document.getElementById("cidade").value;
+  const prova1 = parseFloat(document.getElementById("prova1").value);
+  const prova2 = parseFloat(document.getElementById("prova2").value);
+
+  console.log('name = ' + name)
+  console.log('cpf = ' + cpf)
+  console.log('nascimento = ' + nascimento)
+  console.log('email = ' + email)
+  console.log('telefone = ' + telefone)
+  console.log('pais = ' + pais)
+  console.log('estado = ' + estado)
+  console.log('cidade = ' + cidade)
+  console.log('prova 1 = ' + prova1)
+  console.log('prova 2 = ' + prova2)
+
+  console.log('media = ' + ((prova1 + prova2) /2))
 }
