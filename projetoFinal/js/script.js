50b69b35bcccedd6034b90e58e80c63a5b8fdf45
João Guilherme <joaosilvabarbosa001@gmail.com>
2024-07-31 12:35:11 -0300

    ajustes no html e adição de uma tabela

--- projetoFinal/js/script.js
+++ projetoFinal/js/script.js
@@ -1,7 +1,18 @@
 console.log('Projeto Turma 48');
 
 class Studant {
-  constructor(nome, cpf, nascimento, email, telefone, pais, estado, cidade) {
+  constructor(
+    nome,
+    cpf,
+    nascimento,
+    email,
+    telefone,
+    pais,
+    estado,
+    cidade,
+    prova1,
+    prova2
+  ) {
     this.nome = nome;
     this.cpf = cpf;
     this.nascimento = nascimento;
@@ -10,8 +21,21 @@ class Studant {
     this.pais = pais;
     this.estado = estado;
     this.cidade = cidade;
+    this.prova1 = prova1;
+    this.prova2 = prova2;
+    this.media = (prova1 + prova2) / 2;
+    this.situacao = this.media >= 5 ? 'aprovado' : 'reprovado';
+  }
+
+  exibirDados() {
+    return `O Aluno ${this.nome} tirou a nota ${this.prova1} na AP1 e ${
+      this.prova2
+    } na AP2. Com média de ${this.media.toString().replace('.', ',')} e está ${
+      this.situacao
+    } `;
   }
 }
+
 function enviarFormulario() {
   const nome = document.getElementById('nome').value;
   const cpf = document.getElementById('cpf').value;
@@ -24,7 +48,17 @@ function enviarFormulario() {
   const prova1 = parseFloat(document.getElementById('prova1').value);
   const prova2 = parseFloat(document.getElementById('prova2').value);
 
-  const student = new Studant{
-    name
-  }
+  const student = new Studant(
+    nome,
+    cpf,
+    nascimento,
+    email,
+    telefone,
+    pais,
+    estado,
+    cidade,
+    prova1,
+    prova2
+  );
+  console.log(student.exibirDados());
 }
