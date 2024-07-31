f96097e5805d3573662da55e03e211dda4366669
João Guilherme <joaosilvabarbosa001@gmail.com>
2024-07-31 11:11:28 -0300

    ajustes,adicionando  método construtor

--- projetoFinal/js/script.js
+++ projetoFinal/js/script.js
@@ -1,27 +1,30 @@
-console.log("Projeto Turma 48");
+console.log('Projeto Turma 48');
 
+class Studant {
+  constructor(nome, cpf, nascimento, email, telefone, pais, estado, cidade) {
+    this.nome = nome;
+    this.cpf = cpf;
+    this.nascimento = nascimento;
+    this.email = email;
+    this.telefone = telefone;
+    this.pais = pais;
+    this.estado = estado;
+    this.cidade = cidade;
+  }
+}
 function enviarFormulario() {
-  const name = document.getElementById("name").value;
-  const cpf = document.getElementById("cpf").value;
-  const nascimento = document.getElementById("nascimento").value;
-  const email  = document.getElementById("email").value;
-  const telefone = document.getElementById("telefone").value;
-  const pais = document.getElementById("pais").value;
-  const estado = document.getElementById("estado").value;
-  const cidade = document.getElementById("cidade").value;
-  const prova1 = parseFloat(document.getElementById("prova1").value);
-  const prova2 = parseFloat(document.getElementById("prova2").value);
+  const nome = document.getElementById('nome').value;
+  const cpf = document.getElementById('cpf').value;
+  const nascimento = document.getElementById('nascimento').value;
+  const email = document.getElementById('email').value;
+  const telefone = document.getElementById('telefone').value;
+  const pais = document.getElementById('pais').value;
+  const estado = document.getElementById('estado').value;
+  const cidade = document.getElementById('cidade').value;
+  const prova1 = parseFloat(document.getElementById('prova1').value);
+  const prova2 = parseFloat(document.getElementById('prova2').value);
 
-  console.log('name = ' + name)
-  console.log('cpf = ' + cpf)
-  console.log('nascimento = ' + nascimento)
-  console.log('email = ' + email)
-  console.log('telefone = ' + telefone)
-  console.log('pais = ' + pais)
-  console.log('estado = ' + estado)
-  console.log('cidade = ' + cidade)
-  console.log('prova 1 = ' + prova1)
-  console.log('prova 2 = ' + prova2)
-
-  console.log('media = ' + ((prova1 + prova2) /2))
+  const student = new Studant{
+    name
+  }
 }
